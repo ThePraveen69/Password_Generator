8a8e12e5c39d122139b2c31161b0b1df36af3cfb
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-03-30 17:56:21 +0530

    Implement password generation function
    Add password generation function with options for numbers and special characters.

--- Pass_Gen.py
+++ Pass_Gen.py
@@ -1,2 +1,16 @@
 import random
 import string
+def generate_password(min_length,numbers = True,special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters +=special
+
+    return characters
+pwd = generate_password(10)  
+print(pwd)
