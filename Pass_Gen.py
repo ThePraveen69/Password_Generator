bb3f2c87f4231202f0119d17229a3d3c62c42053
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-03-31 21:40:59 +0530

    Enhance password generation criteria checks
    Added checks for numbers and special characters in password generation.

--- Pass_Gen.py
+++ Pass_Gen.py
@@ -18,7 +18,19 @@ def generate_password(min_length,numbers = True,special_characters=True):
     while not meets_criteria or len(pwd) < min_length:
         new_char = random.choice(characters)
         pwd +=new_char
+    
+    if new_char in digits:
+        has_numbers = True
+    elif new_char in special:
+        has_special = True
+    
+    meets_criteria = True
+    if numbers :
+        meets_criteria = has_number
+    if special_characters:
+        meets_criteria = meets_criteria and has_special
 
+    return pwd
 
 pwd = generate_password(10)  
 print(pwd)
