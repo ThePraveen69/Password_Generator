73516bff2a730f1505ad880d8f0f1ee94dae11a9
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-03-31 20:51:10 +0530

    Enhance password generation logic
    Implement password generation with criteria checks.

--- Pass_Gen.py
+++ Pass_Gen.py
@@ -11,6 +11,15 @@ def generate_password(min_length,numbers = True,special_characters=True):
     if special_characters:
         characters +=special
 
-    return characters
+    pwd =""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd +=new_char
+
+
 pwd = generate_password(10)  
 print(pwd)
+
